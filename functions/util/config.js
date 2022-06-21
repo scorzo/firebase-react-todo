80ee0e319cb67811734445e1e78d64589a90ad8f
tyssean <seancorzo@tyscreative.com>
2022-06-21 11:38:31 -0700

    first

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+module.exports = {
+    apiKey: "AIzaSyDcKkXeKYXVFjAmK7inRDmVGk1KG9QbbB4",
+    authDomain: "fir-todo-968a5.firebaseapp.com",
+    projectId: "fir-todo-968a5",
+    storageBucket: "fir-todo-968a5.appspot.com",
+    messagingSenderId: "614024324341",
+    appId: "1:614024324341:web:ac5c8e2ec0196bffbd91bc",
+    measurementId: "G-Z57T7VHESM"
+};
